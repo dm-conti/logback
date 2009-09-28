db9b38fcf29dffd1e22d11be4b277f783004547e
rgoers <rgoers@rgoerss-macbook-pro.local>
2009-09-28 14:33:30 -0700

    Fix LBCLASSIC-154

---
+++ logback-classic/src/test/java/ch/qos/logback/classic/issue/lbclassic154/LoggingAppender.java
@@ -0,0 +1,12 @@
+package ch.qos.logback.classic.issue.lbclassic154;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: rgoers
+ * Date: Sep 28, 2009
+ * Time: 2:11:07 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LoggingAppender
+{
+}
